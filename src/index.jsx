6080ecb9d60a53a69e75e834913de32a432886f4
Laura F. García <52688109+LauraFgar@users.noreply.github.com>
2023-05-12 17:07:06 -0500

    Update index.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -3,6 +3,8 @@ import LearningHeader from './learning-header/LearningHeader';
 import messages from './i18n/index';
 import StudioHeader from './StudioHeader';
 
+console.log('HEADER GW');
+
 export { LearningHeader, messages, StudioHeader };
 
 export default Header;
